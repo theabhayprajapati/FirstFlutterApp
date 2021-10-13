86c8eea0deb17bacdbf930406bf27600fd880df9
theabhayprajapati <theabhayprajapati@gmail.com>
2021-10-13 18:31:30 +0530

    fluttertwo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertwodotfive/pages/home_page.dart';
+import 'package:fluttertwodotfive/pages/login_page.dart';
 
 void main() {
   runApp(MyApp()); //use runApp to write a app name
@@ -13,9 +14,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme:
+          ThemeData(brightness: Brightness.dark, primarySwatch: Colors.yellow),
+      // initialRoute: "/HomePage", ---- if you want to make any page as inital page do this
+      routes: {
+        "/": (context) => Loginpage(),
+        "/HomePage": (context) => HomePage(),
+      },
     );
   }
 }
 //here build is very important in overriding function as it handles the function of UI in the app, which means that
 //this is responsible for any future UI changes inthe app:
+// day 2 completed
+
+
+// day 3 completed
+
+
+
+
+
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Loginpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(child: Column());
+  }
+}
